dc56975608c59536e46b9daee12177a927afde70
mova <valeriy.molchanov.77@gmail.com>
2020-10-19 17:46:18 +0300

    goingserver reflection

--- goingserver/server.go
+++ goingserver/server.go
@@ -8,11 +8,13 @@ import (
 	pb "github.com/movaua/going/going"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func newServer() *grpc.Server {
 	server := grpc.NewServer()
 	pb.RegisterGreeterServer(server, &greeterServer{})
+	reflection.Register(server)
 	return server
 }
 
